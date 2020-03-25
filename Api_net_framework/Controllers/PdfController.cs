a08a000f07392de4cf91d08dd75b7ffc29069c3c
Jasson <jasson142@gmail.com>
2020-03-24 19:06:33 -0500

    adding pdf functionallity on the way ! letf to do the correct format !

--- Api_net_framework/Controllers/PdfController.cs
+++ Api_net_framework/Controllers/PdfController.cs
@@ -5,9 +5,12 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Api.Models;
-using Pechkin;
 using System.Web;
 using RouteAttribute = System.Web.Http.RouteAttribute;
+using System.Drawing;
+using PdfSharp.Pdf;
+using TheArtOfDev.HtmlRenderer.PdfSharp;
+using PdfSharp;
 
 namespace Api.Controllers
 {
@@ -20,25 +23,34 @@ namespace Api.Controllers
         [HttpPost]
         public string CreatePdf(Configurator configurator)
         {
-            GlobalConfig options = new GlobalConfig();
-
-            options.SetMargins(0, 0, 0, 0)
-                .SetDocumentTitle("ahun no se we")
-                .SetPaperSize(System.Drawing.Printing.PaperKind.Letter);
+            PdfDocument pdfDocument = new PdfDocument();
             string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Scripts\test");
             string html = File.ReadAllText(HttpContext.Current.Server.MapPath("~/Views/Home/test.cshtml"));
-            html.Replace("{name}", configurator.Name);
-            html.Replace("{print}", configurator.print);
-            //string path = @"~/Views/Home/test.cshtml";
-            //return html;
-            byte[] pdfContent = new SimplePechkin(options).Convert(html);
-
-            //string directory = "C:\\Users\\Juan\\Desktop\\";
-            // Name of the PDF
-            
+            html.Replace("{name}", @"TEST");
+            html = html.Replace("{print}", Path.Combine(HttpContext.Current.Server.MapPath("~"), @"wwwroot\" + configurator.print));
+            var cssData = PdfGenerator.ParseStyleSheet(@".invoice-container { max-width: 800px; margin: auto; padding: 20px; font-size: 15px; line-height: 20px; font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; color: #555; } .invoice-container table { width: 100%; line-height: inherit; text-align: left; } .invoice-container table td { padding: 2px; vertical-align: middle; } .invoice-container table tr td:nth-child(2) { text-align: left; } .invoice-container table tr.top table td { padding-bottom: 20px; } .invoice-container table tr.top table td.title { font-size: 45px; line-height: 45px; color: #333; } .invoice-container table tr.heading td { background: #eee; border-bottom: 1px solid #ddd; font-weight: bold; } .invoice-container table tr.details td { padding-bottom: 20px; } .invoice-container table tr.item td { border-bottom: 1px solid #eee; } .invoice-container table tr.item.last td { border-bottom: none; } .invoice-container table tr.total td:nth-child(2) { border-top: 2px solid #eee; font-weight: bold; } .productImg { margin: 0px 0px 0px -300px; width: 1000px; } .detailed td { border-bottom: 1px solid #eee; text-align: left; } ul { margin: 0; list-style: none; padding: 0; } .logo-image { width: 50%; }", false);
+            PdfDocument pdf = PdfGenerator.GeneratePdf(html, PageSize.A4, 60, cssData);
             string filename = Guid.NewGuid() + ".pdf";
-            File.WriteAllBytes(Path.Combine(HttpContext.Current.Server.MapPath("~"), @"wwwroot\" + filename), pdfContent);
+            pdf.Save(Path.Combine(HttpContext.Current.Server.MapPath("~"), @"wwwroot\" + filename));
+
             return filename;
+
+            /* GlobalConfig options = new GlobalConfig();
+
+             options.SetMargins(0, 0, 0, 0)
+                 .SetDocumentTitle("ahun no se we")
+                 .SetPaperSize(System.Drawing.Printing.PaperKind.Letter);
+
+             //string path = @"~/Views/Home/test.cshtml";
+             //return html;
+             byte[] pdfContent = new SimplePechkin(options).Convert(html);
+
+             //string directory = "C:\\Users\\Juan\\Desktop\\";
+             // Name of the PDF
+
+             string filename = Guid.NewGuid() + ".pdf";
+             File.WriteAllBytes(Path.Combine(HttpContext.Current.Server.MapPath("~"), @"wwwroot\" + filename), pdfContent);
+             return filename;*/
         }
 
         public string Get()
@@ -48,7 +60,7 @@ namespace Api.Controllers
 
         [Route("api/image")]
         [HttpPost]
-        public HttpResponseMessage UploadImage(Image postedFile)
+        public HttpResponseMessage UploadImage(PdfImage postedFile)
         {
             Dictionary<string, object> dict = new Dictionary<string, object>();
             
